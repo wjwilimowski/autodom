01f9c6c5b6ca6972b3212cef7fd444fb02e31db2
wwilim <wjwilimowski@gmail.com>
2024-08-16 00:54:27 +0200

    Cleanup

--- autodom/Program.cs
+++ autodom/Program.cs
@@ -11,41 +11,28 @@ var configuration = new ConfigurationBuilder()
 var user = int.Parse(configuration["TMD_USER"]!);
 var pass = configuration["TMD_PASS"]!;
 
-using var api = new TmdApi(user, pass);
-await api.LoginAsync();
-var pdfs = await api.GetMonthlyPdfsAsync();
-
 if (Directory.Exists("out"))
 {
     Directory.Delete("out", recursive: true);
 }
 Directory.CreateDirectory("out");
+
+using var api = new TmdApi(user, pass);
+await api.LoginAsync();
+var pdfs = await api.GetMonthlyPdfsAsync();
 foreach (var item in pdfs.Where(x => x != null))
 {
     Console.WriteLine(item);
     var path = "out\\" + item.Title.Replace('/', '-') + ".pdf";
-    Console.WriteLine(path);
     using var printout = await api.GetPrintoutAsync(item);
     using var fs = File.Create(path);
     await printout.CopyToAsync(fs);
 }
 
-async Task<string> GetToken(HttpClient httpClient)
-{
-
-    var response = await httpClient.PostAsync(new Uri("https://main.tomojdom.pl/login/OsLogInPass"), new StringContent("{\"User\":15734796,\"Pass\":\"!!Jaro9c9\"}"));
-
-    var json = await response.Content.ReadAsStringAsync();
-    var data = JsonSerializer.Deserialize<object[]>(json);
-
-    var token = data[2].ToString();
-    return token;
-}
-
 class TmdApi : IDisposable
 {
     private readonly HttpClient httpClient = new();
-    private string _token;
+    private string? _token;
     private readonly int _user;
     private readonly string _pass;
     private readonly int _year = DateTime.Now.Year;
@@ -78,10 +65,8 @@ class TmdApi : IDisposable
         var response = await httpClient.PostAsync(new Uri("https://taurus.tomojdom.pl/app/api/RozliczeniaSzczegolowe"), StringContent(new { WId = 15, Rok = _year}));
 
         var json = await response.Content.ReadAsStringAsync();
-        var shitData = JsonSerializer.Deserialize<object[][]>(json)!;
-        var saneData = shitData.Select(ParseThisShit).ToList();
 
-        return saneData;
+        return JsonSerializer.Deserialize<object[][]>(json)!.Select(Parse).ToList();
     }
 
     public async Task<Stream> GetPrintoutAsync(MonthlyPdf pdf)
@@ -95,22 +80,20 @@ class TmdApi : IDisposable
         return new StringContent(JsonSerializer.Serialize(o));
     }
 
-    private static MonthlyPdf? ParseThisShit(object[] shit)
+    private static MonthlyPdf Parse(object[] item)
     {
-        var mid = shit[0].ToString()!;
-        var month = DateTime.Parse(shit[1].ToString()!);
+        var mid = item[0].ToString()!;
+        var month = DateTime.Parse(item[1].ToString()!);
         if (month.Year == 1000)
         {
-            return null;
+            return null!;
         }
-        var moreShit = (JsonElement)shit[2];
-        var evenMoreShit = moreShit![0];
-        var date = DateTime.Parse(evenMoreShit![0].ToString()!);
-        var title = evenMoreShit[1].ToString()!;
-        var amount = evenMoreShit[2].GetDecimal();
-        var id = evenMoreShit[3].GetInt32();
+        var innerItem = ((JsonElement)item[2])![0];
+        var date = DateTime.Parse(innerItem![0].ToString()!);
+        var title = innerItem[1].ToString()!;
+        var amount = innerItem[2].GetDecimal();
+        var id = innerItem[3].GetInt32();
         
-
         return new MonthlyPdf
         {
             Id = id,
