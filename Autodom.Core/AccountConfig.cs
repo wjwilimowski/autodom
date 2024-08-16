1cc99eea196d358223e8560f92aff778f6b45969
wwilim <wjwilimowski@gmail.com>
2024-08-16 23:59:45 +0200

    AccountConfig

---
+++ Autodom.Core/AccountConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace Autodom.Core;
+
+public record AccountConfig
+{
+    public required int User { get; init; }
+    public required string Pass { get; init; }
+    public required List<string> EmailsToNotify { get; init; } = [];
+}
